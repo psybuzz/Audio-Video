5455a38abf9c1e7010a7d9460e6ebe219467c660
mlli2 <mlli2@illinois.edu>
2014-11-13 04:46:00 -0600

    Fixed stats, some small styling changes

--- index.js
+++ index.js
@@ -30,10 +30,23 @@ $(document).ready(function() {
 	});
 
 
+
 	video.addEventListener('loadeddata', function() {
+		var max = Math.max.apply(Math, totoroColors)
+		var min = Math.min.apply(Math, totoroColors)
+		max-=min
+		var average = 0;
+		for (var i = 0; i <totoroColors.length; i++)
+		{
+			//System.out.println("Luminence," + i*5 + "," + (luminence[i]-min)/max);
+            totoroColors[i] = (totoroColors[i]-min)/max;
+            average +=totoroColors[i];
+		}
+		average /= totoroColors.length
+
 		$("#statsList").html("<li><p>Length: " + video.duration + " seconds</p></li>" +
 			"<li><p>IMDB Rating: 8.3</p></li>" +
-	        "<li><p>Average luminence (0 is dark, 1 is bright): 0.5259608192051154 </p></li>" +
+	        "<li><p>Average luminence (0 is dark, 1 is bright): " + average + " </p></li>" +
 	        "<li><p>Average Sentiment (0 is negative, 1 is positive): " + averageSentiment+ "</p></li>")
 	});
 
